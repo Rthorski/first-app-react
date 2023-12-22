74765eda1637ae3012a889539e367353298714be
Rthorski <alexandre.rethore@icloud.com>
2023-12-22 12:31:18 +0100

    set dynamic cards

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,30 @@
 function PokemonCard() {
+    const pokemon = pokemonList[0];
     return (
         <>
             <figure>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+                {pokemon.imgSrc ? (
+                    
+                    <img src={pokemon.imgSrc} alt={pokemon.name} />
+                ) : (
+                    <p>???</p>
+                )}
+                <figcaption>{pokemon.name}</figcaption>
             </figure>
-            <figcaption>Bulbausaur</figcaption>
         </>
     );
 }
 
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",       
+    },
+    {
+        name: "mew",
+    },
+];
+
 export default PokemonCard;
+
